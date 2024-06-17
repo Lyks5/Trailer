bc4acdb82d191733606949c1260951c7bf2c28aa
Нуралиев Хабибилло Хамидиллоевич <21067@iat.iat>
2024-06-17 14:58:51 +0800

    17.06

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>{{ config('app.name', 'Lar') }}</title>
+    
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="icon" href="./favicon.ico" />
     @vite('resources/css/app.css')
 </head>
 <style>
